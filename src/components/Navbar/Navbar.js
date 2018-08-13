5a2a5471759675b31054b031b5ceee01192d36ab
Pranav Pillai <pillaip@seas.upenn.edu>
2018-08-13 12:47:21 -0400

    removes junk from router

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import BadgeList from '../Badges/BadgeList';
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <div>
+        {
+          Object.keys(this.props.badges).length > 0 && <BadgeList badges={this.props.badges} />
+        }
+      </div>
+    );
+  }
+}
+const mapStateToProps = (state) => {
+  return {
+    badges: state.web3.badges,
+  }
+}
+
+export default connect(mapStateToProps)(Navbar);
