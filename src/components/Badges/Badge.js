fcbf5ac73f28ec4c3060fb73d56bb01094a46a20
Pranav Pillai <pillaip@seas.upenn.edu>
2018-08-13 10:52:12 -0400

    adds Link to badges

--- src/components/Badges/Badge.js
+++ src/components/Badges/Badge.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
 import './styles.css';
 
 class Badge extends React.Component {
   render() {
-    const {badge: {image: {contentURL: imgSrc}, name}} = this.props;
+    const {badge: {image: {contentURL: imgSrc}, name, id}} = this.props;
     return (
-      <div >
-      <img src={imgSrc} alt='Artist Badge' className='badge-image'/>
-      <p className='badge-name'> {name} </p>
-      </div>
+      <Link to={'/' + id}>
+        <div>
+        <img src={imgSrc} alt='Artist Badge' className='badge-image'/>
+        <p className='badge-name'> {name} </p>
+        </div>
+      </Link>
     );
   }
 }
