89f8f836f10fa5cadcd712c942ba36c9fb9f29eb
Pranav Pillai <pillaip@seas.upenn.edu>
2018-08-14 13:13:30 -0400

    handles messages appropriately

--- src/components/ChatBox/ChatBox.js
+++ src/components/ChatBox/ChatBox.js
@@ -8,24 +8,22 @@ const Col = visComp.Col;
 class ChatBox extends React.Component {
   render() {
     return (
-      <div className="fan-page">
-        <div className="container">
-          <Row>
-            <Col xs={12} sm={12} md={6} lg={6}>
-              <form>
-                <input type="text" value={this.props.input} onChange={this.props.onChange}/>
-                <button type="submit" onClick={this.props.onSubmit}>Hey</button>
-              </form>
-              <div className="messages">
-                {
-                  this.props.messages.map((messageObj) => {
-                    return messageObj.message;
-                  })
-                }
-              </div>
-            </Col>
-          </Row>
-        </div>
+      <div className="chat-box">
+        <Row>
+          <Col xs={12} sm={12} md={6} lg={6}>
+            <form>
+              <input type="text" value={this.props.input} onChange={this.props.onChange}/>
+              <button type="submit" onClick={this.props.onSubmit}>Hey</button>
+            </form>
+            <div className="messages">
+              {
+                this.props.messages.map((messageObj) => {
+                  return messageObj.message;
+                })
+              }
+            </div>
+          </Col>
+        </Row>
       </div>
     );
   }
