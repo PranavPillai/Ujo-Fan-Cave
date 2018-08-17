44d6ac0e5142a3cbdcfec70a2c710791f10cf218
Pranav Pillai <pillaip@seas.upenn.edu>
2018-08-17 07:08:58 -0400

    starts fan page default styling

--- src/pages/FanPageGeneric.js
+++ src/pages/FanPageGeneric.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import visComp from 'ujo-style-guide';
 import Navbar from '../components/Navbar/Navbar';
+import { Segment } from 'semantic-ui-react';
 
 const Row = visComp.Row;
 const Col = visComp.Col;
@@ -15,7 +16,7 @@ const FanPageGeneric = () =>
           <Col lg={9} lgOffset={1}>
             <Row className="section-header">
               <Col xs={12} sm={12} md={6} lg={6}>
-                <h1>Welcome to the beat! Explore your badges on the left</h1>
+                <Segment raised><h1>Welcome to the beat! Explore your badges on the left</h1></Segment>
               </Col>
             </Row>
           </Col>
