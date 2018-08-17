a253b5df06d3655774cb7b12c8829cdf67c63748
Pranav Pillai <pillaip@seas.upenn.edu>
2018-08-17 05:50:47 -0400

    styles home page

--- src/components/Dashboard/Content.js
+++ src/components/Dashboard/Content.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import classNames from 'classnames';
-import { Container, Segment } from 'semantic-ui-react';
+import { Segment } from 'semantic-ui-react';
 
 class Content extends React.Component {
   render() {
     const segmentClassNames = classNames('post', {hidden: !this.props.notHidden});
     return (
-      // <div className="content-container">
-      //   <li className={contentClassNames}>
-      //     <sup>{this.props.content.name}</sup>
-      //     <p>{this.props.content.content}</p>
-      //   </li>
-      // </div>
       <div className={segmentClassNames}>
         <Segment>
           <sup>{this.props.content.time}</sup>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -14,6 +14,7 @@ import {
   Segment,
   Visibility,
 } from 'semantic-ui-react';
+import cheers from '../assets/cheers.png';
 import './homepage.css';
 
 /* eslint-disable react/no-multi-comp */
@@ -90,7 +91,6 @@ import './homepage.css';
                   <Menu.Item as='a' active>
                     Home
                   </Menu.Item>
-                  <Link to="/about" className="item">About</Link>
                   <Link to="/pages" className="item">Fan Pages</Link>
                   <Menu.Item position='right'>
                     <a href="http://ujomusic.com/">
@@ -115,98 +115,59 @@ class HomePage extends React.Component {
   render = () => (
     <DesktopContainer>
       <Segment style={{ padding: '8em 0em' }} vertical>
-        <Grid container stackable verticalAlign='middle'>
-          <Grid.Row>
-            <Grid.Column width={8}>
-              <Header as='h3' style={{ fontSize: '2em' }}>
-                We Help Companies and Companions
-              </Header>
-              <p style={{ fontSize: '1.33em' }}>
-                We can give your company superpowers to do things that they never thought possible.
-                Let us delight your customers and empower your needs... through pure data analytics.
-              </p>
-              <Header as='h3' style={{ fontSize: '2em' }}>
-                We Make Bananas That Can Dance
-              </Header>
-              <p style={{ fontSize: '1.33em' }}>
-                Yes that's right, you thought it was the stuff of dreams, but even bananas can be
-                bioengineered.
-              </p>
-            </Grid.Column>
-            <Grid.Column floated='right' width={6}>
-              <Image bordered rounded size='large' src='/images/wireframe/white-image.png' />
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Column textAlign='center'>
-              <Button size='huge'>Check Them Out</Button>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
-      <Segment style={{ padding: '8em 0em' }} vertical>
         <Container text>
           <Header as='h3' style={{ fontSize: '2em' }}>
-            Breaking The Grid, Grabs Your Attention
+            Connecting Artists and Fans like Never Before
           </Header>
           <p style={{ fontSize: '1.33em' }}>
-            Instead of focusing on content creation and hard work, we have learned how to master the
-            art of doing nothing by providing massive amounts of whitespace and generic content that
-            can seem massive, monolithic and worth your attention.
+            The Beat is your home to connect with your favorite artist,
+            where super-fans like yourself can be rewarded with exclusive content & deals,
+            and can chat with people as passionate about music as they are. 
           </p>
-          <Button as='a' size='large'>
-            Read More
-          </Button>
           <Divider
             as='h4'
             className='header'
             horizontal
             style={{ margin: '3em 0em', textTransform: 'uppercase' }}
           >
-            <a href='#'>Case Studies</a>
           </Divider>
           <Header as='h3' style={{ fontSize: '2em' }}>
-            Did We Tell You About Our Bananas?
+            Powered by the Blockchain
           </Header>
           <p style={{ fontSize: '1.33em' }}>
-            Yes I know you probably disregarded the earlier boasts as non-sequitur filler content, but
-            it's really true. It took years of gene splicing and combinatory DNA research, but our
-            bananas can really dance.
+            Built using Ujo Music's decentralized music registry, The Beat supports 
+            unencumbered musicians, eager to break the shackles of the music industry. 
+            Through smart-contracts on the Ethereum Blockchain, Ujo helps artists control
+            their intellectual property without paying a huge cut to centralized distribution channels. 
           </p>
-          <Button as='a' size='large'>
-            I'm Still Quite Interested
-          </Button>
+          <Divider
+            as='h4'
+            className='header'
+            horizontal
+            style={{ margin: '3em 0em', textTransform: 'uppercase' }}
+          />
+          <Header as='h3' style={{ fontSize: '2em' }}>
+            Interested in Learning More? 
+          </Header>
+          <p style={{ fontSize: '1.33em' }}>
+            Click the button below to check out Ujo Music, where you can buy the collectible badges
+            that power this platform.
+          </p>
+          <Button size='huge' color="pink"><a style={{"text-decoration": "none", color: "white"}} href="www.ujomusic.com">Visit Ujo Music</a></Button>
         </Container>
       </Segment>
       <Segment inverted vertical style={{ padding: '5em 0em' }}>
         <Container>
           <Grid divided inverted stackable>
-            <Grid.Row>
-              <Grid.Column width={3}>
-                <Header inverted as='h4' content='About' />
-                <List link inverted>
-                  <List.Item as='a'>Sitemap</List.Item>
-                  <List.Item as='a'>Contact Us</List.Item>
-                  <List.Item as='a'>Religious Ceremonies</List.Item>
-                  <List.Item as='a'>Gazebo Plans</List.Item>
-                </List>
-              </Grid.Column>
-              <Grid.Column width={3}>
-                <Header inverted as='h4' content='Services' />
-                <List link inverted>
-                  <List.Item as='a'>Banana Pre-Order</List.Item>
-                  <List.Item as='a'>DNA FAQ</List.Item>
-                  <List.Item as='a'>How To Access</List.Item>
-                  <List.Item as='a'>Favorite X-Men</List.Item>
-                </List>
-              </Grid.Column>
-              <Grid.Column width={7}>
-                <Header as='h4' inverted>
-                  Footer Header
-                </Header>
-                <p>
-                  Extra space for a call to action inside the footer that could help re-engage users.
+            <Grid.Row centered>
+              <Grid.Column width={4} />
+              <Grid.Column width={8} textAlign="center">
+                <p style={{margin: 'auto',}}>
+                  Cheers <img src={cheers} style={{width: '20px'}}/> --&nbsp;Crystal Desai, Michelle Wu, and Pranav Pillai
                 </p>
+                <p>Summer 2018</p>
+              </Grid.Column>
+              <Grid.Column width={4}>
               </Grid.Column>
             </Grid.Row>
           </Grid>
